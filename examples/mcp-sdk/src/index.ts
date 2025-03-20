e3054422f94992101567210e9a393b1093857be2
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2025-03-20 08:16:15 +0530

    chore: updated the example

--- examples/mcp-sdk/src/index.ts
+++ examples/mcp-sdk/src/index.ts
@@ -1,11 +1,19 @@
 import { StdioServerTransport } from "./transport";
 import { Hono } from "hono";
-import { muppet, describeTool, validator } from "muppet";
+import {
+  muppet,
+  describeTool,
+  staticResource,
+  dynamicResource,
+  describePrompt,
+  validator,
+} from "muppet";
 import z from "zod";
 import pino from "pino";
 
 const app = new Hono();
 
+// Define a simple hello world tool
 app.post(
   "/",
   describeTool({
@@ -28,6 +36,60 @@ app.post(
         },
       ],
     });
+  },
+);
+
+// Define static resources
+app.post(
+  "/static/*",
+  staticResource({
+    name: "Static Resource",
+    description: "A static resource",
+    resource: {
+      path: "E:/dev/muppet/muppet/examples/mcp-sdk/dist/static",
+    },
+  }),
+);
+
+// Define Dynamic resources
+app.post(
+  "/dynamic/*",
+  dynamicResource({
+    name: "Dynamic Resource",
+    description: "A dynamic resource",
+  }),
+  (c) => {
+    return c.json([
+      {
+        uri: "file:///logs/app.log",
+        name: "Application Logs",
+        mimeType: "text/plain",
+      },
+    ]);
+  },
+);
+
+// Define a simple prompt
+app.post(
+  "/propmt",
+  validator(
+    "json",
+    z.object({
+      name: z.string(),
+    }),
+  ),
+  describePrompt({ name: "Simple Prompt" }),
+  (c) => {
+    const { name } = c.req.valid("json");
+    return c.json([
+      {
+        role: "user",
+        content: {
+          type: "text",
+          text: `This is a simple prompt for ${name}`,
+        },
+      },
+    ]);
   },
 );
 
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,6 @@
 export * from "./tools/index.js";
+export * from "./resources/index.js";
+export * from "./prompts/index.js";
 export { muppet } from "./muppet.js";
 export type * from "./types.js";
 export * from "./utils.js";
