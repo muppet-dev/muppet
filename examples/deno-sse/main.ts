5fbe9723f9298d4302f40d66660206a57eada875
Aditya Mathur <mathuraditya724@gmail.com>
2025-04-02 12:55:14 +0530

    feat: added deno examples

---
+++ examples/deno-sse/main.ts
@@ -0,0 +1,77 @@
+import { Hono } from "hono";
+import {
+  type ToolResponseType,
+  bridge,
+  describeTool,
+  mValidator,
+  muppet,
+} from "muppet";
+import { SSEHonoTransport, streamSSE } from "muppet/streaming";
+import z from "zod";
+
+const app = new Hono();
+
+app.post(
+  "/hello",
+  describeTool({
+    name: "Greet User with Hello",
+    description:
+      "This will take in the name of the user and greet them. eg. Hello John",
+  }),
+  mValidator(
+    "json",
+    z.object({
+      name: z.string(),
+    }),
+  ),
+  (c) => {
+    const { name } = c.req.valid("json");
+    return c.json<ToolResponseType>([
+      {
+        type: "text",
+        text: `Hello ${name}!`,
+      },
+    ]);
+  },
+);
+
+const mcp = muppet(app, {
+  name: "My Muppet",
+  version: "1.0.0",
+});
+
+/**
+ * For SSE transport
+ */
+let transport: SSEHonoTransport | null = null;
+
+const server = new Hono();
+
+server.get("/sse", (c) => {
+  c.header("Content-Encoding", "Identity");
+  return streamSSE(c, async (stream) => {
+    transport = new SSEHonoTransport("/messages");
+    transport.connectWithStream(stream);
+
+    await bridge({
+      mcp,
+      transport,
+    });
+  });
+});
+
+server.post("/messages", async (c) => {
+  if (!transport) {
+    throw new Error("Transport not initialized");
+  }
+
+  await transport.handlePostMessage(c);
+  return c.text("ok");
+});
+
+server.onError((err, c) => {
+  console.error(err);
+  return c.body(err.message, 500);
+});
+
+Deno.serve(server.fetch);
---
+++ examples/deno-stdio/main.ts
@@ -0,0 +1,47 @@
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { Hono } from "hono";
+import {
+  type ToolResponseType,
+  bridge,
+  describeTool,
+  mValidator,
+  muppet,
+} from "muppet";
+import z from "zod";
+
+const app = new Hono();
+
+app.post(
+  "/hello",
+  describeTool({
+    name: "Greet User with Hello",
+    description:
+      "This will take in the name of the user and greet them. eg. Hello John",
+  }),
+  mValidator(
+    "json",
+    z.object({
+      name: z.string(),
+    }),
+  ),
+  (c) => {
+    const { name } = c.req.valid("json");
+    return c.json<ToolResponseType>([
+      {
+        type: "text",
+        text: `Hello ${name}!`,
+      },
+    ]);
+  },
+);
+
+// Creating a mcp using muppet
+const mcp = muppet(app, {
+  name: "My Muppet",
+  version: "1.0.0",
+});
+
+bridge({
+  mcp,
+  transport: new StdioServerTransport(),
+});
